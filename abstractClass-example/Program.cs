62dc49b82cb5eefac8051dd159d78025293b7f1c
Mateusz Gorzynski <mateuszgorzynski@hotmail.com>
2020-07-11 19:18:04 +0200

    add abstract class

---
+++ abstractClass-example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace abstractClass_example
+{
+    class Program
+    {
+        abstract class Cars
+        {
+            public abstract string Brand { get; set; }
+            public abstract string GetBrand();
+        }
+
+        class Opel : Cars
+        {
+            public override string Brand { get; set; } = "Opel";
+
+            public override string GetBrand()
+            {
+                return Brand;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Cannot create abstract class
+            //var _cars = new Cars();
+
+            var _opel = new Opel();
+            Console.WriteLine($"Auto brand is {_opel.GetBrand()}");
+        }
+    }
+}
