1a6b2990d02755cb6c91e631dbfab122f875e49e
Mateusz Gorzynski <mateuszgorzynski@hotmail.com>
2020-07-11 21:07:56 +0200

    add partial class and sealed

---
+++ partialClass-example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace partialClass_example
+{
+    class Program
+    {
+
+        public partial class Ford
+        {
+            public int NumberOfDoors { get; set; } = 4;
+
+            public int GetNumberOfDoors()
+            {
+                return NumberOfDoors;
+            }
+        }
+
+        public partial class Ford
+        {
+            public string Brand { get; set; } = "Ford";
+            public string GetBrand()
+            {
+                return Brand;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var _ford = new Ford();
+            Console.WriteLine($"Auto brand is {_ford.GetBrand()}, number of doors = {_ford.GetNumberOfDoors()}");
+        }
+    }
+}
---
+++ sealedClass-example/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace sealedClass_example
+{
+    class Program
+    {
+        public class Cars
+        {
+            public int NumberOfDoors { get; set; } = 4;
+
+            public int GetNumberOfDoors()
+            {
+                return NumberOfDoors;
+            }
+
+            public virtual int SomeFunction()
+            {
+                return 0;
+            }
+        }
+
+        public sealed class Ford : Cars
+        {
+            public string Brand { get; set; } = "Ford";
+            public string GetBrand()
+            {
+                return Brand;
+            }
+
+            sealed public override int SomeFunction()
+            {
+                return 0;
+            }
+
+        }
+
+
+            /* public class MiniFord:Ford 
+             {
+             // Cannot inherent from sealed class and we cannot override sealed function
+
+             public override int SomeFunction()
+             {
+                 return 0;
+             }
+         }*/
+        static void Main(string[] args)
+        {
+            var _ford = new Ford();
+            Console.WriteLine($"Auto brand is {_ford.GetBrand()}, number of doors = {_ford.GetNumberOfDoors()}");
+        }
+    }
+}
